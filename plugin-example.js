a60749ab55ed343b01a15242392cd4f139cdc71a
Leo Singer <leo.singer@ligo.org>
2023-07-18 14:23:30 -0400

    Add example plugin

---
+++ plugin-example.js
@@ -0,0 +1,10 @@
+export default {
+  sandbox: {
+    start() {
+      console.log("starting example sandbox plugin");
+    },
+    end() {
+      console.log("stopping example sandbox plugin");
+    },
+  },
+};
